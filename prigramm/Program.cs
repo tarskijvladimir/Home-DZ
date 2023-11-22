a70bfe0f755908300c8e109e8b2c5e3091b73b6b
vladimir <tarskij-vladimir@yandex.ru>
2023-11-22 21:52:38 +0300

    added file program

---
+++ prigramm/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задаем исходный массив строк
+        string[] inputArray = {"123", "abc", "qwerty", "xy", "z"};
+
+        // Создаем новый массив строк
+        string[] outputArray = new string[inputArray.Length];
+
+        // Заполняем новый массив только строками, длина которых меньше или равна 3 символам
+        int index = 0;
+        foreach (string str in inputArray)
+ {
+            if (str.Length <= 3)
+ {
+                outputArray[index] = str;
+                index++;
+            }
+        }
+
+        // Выводим новый массив на экран
+        Console.WriteLine("Новый массив строк:");
+        for (int i = 0; i < index; i++) {
+            Console.WriteLine(outputArray[i]);
+        }
+
+        Console.ReadKey();
